b9ffdcfae922cfdeec58bfa16cb874153bd3b1a8
Jonathan <32264711+JonaVDM@users.noreply.github.com>
2023-12-02 12:14:39 +0100

    Removed unneeded utils

--- utils/array.go
+++ utils/array.go
@@ -1,29 +1,5 @@
 package utils
 
-func Max(arr []int) int {
-	max := arr[0]
-
-	for _, i := range arr {
-		if i > max {
-			max = i
-		}
-	}
-
-	return max
-}
-
-func Min(arr []int) int {
-	min := arr[0]
-
-	for _, i := range arr {
-		if i < min {
-			min = i
-		}
-	}
-
-	return min
-}
-
 func IntRange(start, stop int) []int {
 	out := make([]int, 0)
 	for i := start; i < stop; i++ {
