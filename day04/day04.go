295b9fea96475a84c11b8ba286110bd7070ccbe9
Jonathan <32264711+JonaVDM@users.noreply.github.com>
2023-12-04 10:35:51 +0100

    Optimzed day 4

--- day04/day04.go
+++ day04/day04.go
@@ -53,9 +53,8 @@ func getScore(input []string) int {
 }
 
 func getRealScore(input []string) int {
-
-	cards := make(map[int]int)
-	queue := make([]int, len(input))
+	cardsAmount := make(map[int]int)
+	total := 0
 
 	for i, line := range input {
 		parts := strings.Split(line, ": ")
@@ -64,37 +63,24 @@ func getRealScore(input []string) int {
 
 		nums := strings.Split(parts[1], " | ")
 
-		idParts := strings.Split(parts[0], " ")
-		id, _ := strconv.Atoi(idParts[len(idParts)-1])
-
 		for _, num := range strings.Split(nums[1], " ") {
-			if n, err := strconv.Atoi(num); err != nil {
-				continue
-			} else {
-				card[n] += 1
+			if n, err := strconv.Atoi(num); err == nil {
+				card[n] = 1
 			}
 		}
 
 		for _, num := range strings.Split(nums[0], " ") {
-			if n, err := strconv.Atoi(num); err != nil {
+			if n, err := strconv.Atoi(num); err == nil {
 				continue
 			} else if _, ok := card[n]; ok {
 				score++
 			}
 		}
 
-		cards[id] = score
-		queue[i] = id
-	}
-
-	total := 0
-	for len(queue) > 0 {
-		id := queue[0]
-		queue = queue[1:]
-		total++
-
-		for i := id + 1; i <= id+cards[id] && i <= len(input); i++ {
-			queue = append(queue, i)
+		cardsAmount[i] += 1
+		total += cardsAmount[i]
+		for j := i + 1; j <= i+score && j < len(input); j++ {
+			cardsAmount[j] += cardsAmount[i]
 		}
 	}
 
