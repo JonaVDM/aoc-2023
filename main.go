5cfe9cc97a96f22a87d3f2533cc5d7619feba5b1
Jonathan <32264711+JonaVDM@users.noreply.github.com>
2023-12-08 10:14:57 +0100

    Update display of data

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"time"
 
 	"github.com/jonavdm/aoc-2023/day01"
 	"github.com/jonavdm/aoc-2023/day02"
 	"github.com/jonavdm/aoc-2023/day03"
 	"github.com/jonavdm/aoc-2023/day04"
-	"github.com/jonavdm/aoc-2023/day05"
+	// "github.com/jonavdm/aoc-2023/day05"
 	"github.com/jonavdm/aoc-2023/day06"
 	"github.com/jonavdm/aoc-2023/day07"
 	"github.com/jonavdm/aoc-2023/day08"
@@ -30,29 +31,39 @@ func main() {
 		{2, day02.Run, "day02"},
 		{3, day03.Run, "day03"},
 		{4, day04.Run, "day04"},
-		{5, day05.Run, "day05"},
+		// {5, day05.Run, "day05"},
 		{6, day06.Run, "day06"},
 		{7, day07.Run, "day07"},
 		{8, day08.Run, "day08"},
 	}
 
+	fmt.Println("╔══════════════════════════════════════════════════════════╗")
+	fmt.Println("║                -- Advent Of Code 2023 --                 ║")
+	fmt.Println("╚══════════════════════════════════════════════════════════╝")
+	fmt.Println()
+	fmt.Println("┏━━━━━┯━━━━━━━━━━━━━━━━━━┯━━━━━━━━━━━━━━━━━━┯━━━━━━━━━━━━━━┓")
+	fmt.Println("┃ Day │ Part One         │ Part Two         │ Time         ┃")
+	fmt.Println("┠─────┼──────────────────┼──────────────────┼──────────────┨")
+
 	for _, runner := range runners {
+		start := time.Now()
+		var out [2]interface{}
+
 		if *onlyDay > 0 && runner.Day == *onlyDay {
 			file := runner.File
 			if *replacedInput != "" {
 				file = *replacedInput
 			}
-			out := runner.Function(file)
-			printOutput(runner.Day, out)
+			out = runner.Function(file)
 		}
 
 		if *onlyDay == -1 {
-			out := runner.Function(runner.File)
-			printOutput(runner.Day, out)
+			out = runner.Function(runner.File)
 		}
-	}
-}
 
-func printOutput(day int, out [2]interface{}) {
-	fmt.Printf("\n--- Day %d ---\nPart One: %v\nPart Two: %v\n", day, out[0], out[1])
+		duration := time.Now().Sub(start)
+
+		fmt.Printf("┃ %2d  │ %16v │ %16v │ %12v ┃\n", runner.Day, out[0], out[1], duration)
+	}
+	fmt.Println("┗━━━━━┷━━━━━━━━━━━━━━━━━━┷━━━━━━━━━━━━━━━━━━┷━━━━━━━━━━━━━━┛")
 }
